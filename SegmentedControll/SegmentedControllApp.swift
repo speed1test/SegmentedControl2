d84811aaea851dd7eea0e78e370a041833b186ea
Edwin <edwin12sv@gmail.com>
2023-12-23 20:34:24 -0600

    Initial Commit

---
+++ SegmentedControll/SegmentedControllApp.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentedControllApp.swift
+//  SegmentedControll
+//
+//  Created by elsalvador on 23/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SegmentedControllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
